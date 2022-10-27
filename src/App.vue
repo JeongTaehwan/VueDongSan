7823493f291d34ad6c67e9677641bbb889a5d831
JeongTaehwan <wjdxoghks11222@naver.com>
2022-10-27 22:21:51 +0900

    Add : 반복문 추가로 메뉴 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,50 @@
-<template></template>
+<template>
+  <div class="menu">
+    <a v-for="Menu in menudata" :key="Menu">{{ Menu }}</a>
+  </div>
+
+  <div>
+    원룸샵
+    <h4>{{ room[0] }} 원룸</h4>
+    <p>{{ price[0] }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ room[1] }} 원룸</h4>
+    <p>{{ price[1] }} 만원</p>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "App",
+  data() {
+    return {
+      room: ["준하우스", "비닐하우스"],
+      price: [50, 100],
+      menudata: ["Home", "About", "Login"],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
